0e4c76f366a3b74f5ca9ddf136978142b1ba771c
relo3 <74118821+relo3@users.noreply.github.com>
2023-07-14 15:25:49 +0100

    frontend integrate with login redirect

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,8 +4,73 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\client;
 
 class ClientController extends Controller
 {
-    //
+    public function showClients()
+    {
+            $clients = client::all();
+        
+        return view('Clients.index', compact('clients'));
+    }
+    public function create()
+    {
+      
+        return view('Clients.create');
+       
+    }
+    
+    public function store(Request $request)
+{
+    $validatedData = $request->validate([
+        'code_client' => 'required',
+        'raison_sociale' => 'required',
+        'telephone' => 'required',
+        'Adresse' => 'required',
+        'localisation' => 'required',
+    ]);
+
+    $validatedData['user_id'] = auth()->user()->id;
+
+    $client = client::create($validatedData);
+
+    return redirect()->route('clients.index');
+}
+    
+    public function edit($id)
+    {
+       
+        $client = client::findOrFail($id);
+     
+        
+        
+        return view('client.edit', compact('client'));
+    }
+    
+    public function update(Request $request,  $id)
+    {
+        $client = client::findOrFail($id);
+
+        $data = $request->only(['code_client', 'raison_sociale', 'telephone','Adresse','localisation']);
+    
+        
+        $article->update($data);
+    
+    
+        
+    
+        $article->update($request->all());
+        return redirect()->route('client.index');
+    }
+    
+    
+    
+    public function destroy(client $client)
+    {
+        $article->delete();
+        return redirect()->route('client.index');
+    }
+    
+
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,7 +75,7 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->user_name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
