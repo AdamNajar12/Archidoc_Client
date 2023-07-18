057a1de2124bff9d57bad14225c656d88b3f1fec
relo3 <74118821+relo3@users.noreply.github.com>
2023-07-18 09:45:16 +0100

    Crud Statuts

--- app/Http/Controllers/Type_intervention_Controller.php
+++ app/Http/Controllers/Type_intervention_Controller.php
@@ -7,5 +7,69 @@ use Illuminate\Http\Request;
 
 class Type_intervention extends Controller
 {
+    public function showStatuts()
+    {
+
+           $Statuts = Statuts::join('users', 'statuts.user_id', '=', 'users.id')
+    ->select('statuts.*', 'users.prenom as user_name','users.nom as second_name')
+    ->get();
+        return view('statuts.index', compact('statuts'));
+    }
+    public function create()
+    {
+      
+        return view('statuts.create');
+       
+    }
     
+    public function store(Request $request)
+{
+    $validatedData = $request->validate([
+        'libelle' => 'required',
+        
+    ]);
+
+    $validatedData['user_id'] = auth()->user()->id;
+
+    $Statut = statut::create($validatedData);
+
+    return redirect()->route('statuts.index');
+}
+    
+    public function edit($id)
+    {
+       
+        $statuts = statut::findOrFail($id);
+     
+        
+        
+        return view('statuts.edit', compact('statuts '));
+    }
+    
+    public function update(Request $request,  $id)
+    {
+        $statuts = statut::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'libelle' => 'required',
+           
+        ]);
+    
+        $data = $request->only(['libelle']);
+        $data['user_id'] = auth()->user()->id;
+    
+        $Application->update($data);
+    
+        return redirect()->route('statuts.index');
+    }
+    
+    
+    
+    public function destroy(statut $staut)
+    {
+        $statut->delete();
+        return redirect()->route('statuts.index');
+    }
+    
+
 }
